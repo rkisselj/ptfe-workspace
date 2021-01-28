187f4fa04dc5b74673b8c131088f32728e0457eb
HashiCorp Training <training@hashicorp.com>
2021-01-28 19:06:37 +0000

    rtk

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ variable "environment" {}
 variable "location" {}
 
 resource "azurerm_resource_group" "main" {
-  name     = "${var.prefix}-tfe-rg"
+  name     = "${var.prefix}-tfe-rg-1"
   location = var.location
 
   tags = {
